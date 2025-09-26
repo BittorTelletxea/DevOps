2070e7b11e3f67240c17aa28ed745f22c5ab3e13
BittorTelletxea <btelecheael22wg@ikzubirimanteo.com>
2025-09-26 14:24:25 +0200

    hhh

--- Test/miapp/src/test/java/com/ejemplo/AppTest.java
+++ Test/miapp/src/test/java/com/ejemplo/AppTest.java
@@ -1,6 +1,6 @@
 package com.ejemplo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
@@ -8,7 +8,7 @@ class AppTest {
     @Test
     void testSumaCorrecta() {
         int resultado = App.suma(2, 3);
-        assertEquals(5, resultado, "La suma debería dar 5");
+        assertEquals(6, resultado, "La suma debería dar 5");
     }
 
 }
