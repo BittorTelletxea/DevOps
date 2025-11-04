5a15b4ab7fb4374e2688869064378ef957202453
BittorTelletxea <btellecheael22wg@ikzubirimanteo.com>
2025-11-04 14:47:50 +0100

    Update Maven package step to skip tests and adjust test class package declaration

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+@SpringBootTest(classes = DemoApiApplication.class)
+class DemoApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
